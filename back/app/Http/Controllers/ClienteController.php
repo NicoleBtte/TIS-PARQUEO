d698e844351d173793738d97c7576ce6f90b8965
NicoleBtte <82354191+NicoleBtte@users.noreply.github.com>
2023-05-02 11:53:21 -0400

    asignacion-funciona-v1

--- back/app/Http/Controllers/ClienteController.php
+++ back/app/Http/Controllers/ClienteController.php
@@ -119,7 +119,7 @@ class ClienteController extends Controller
                 'status' => 1,
                 'message' => 'login correcto',
                 'access_token' => $token,
-                'rol' => 'guardia',
+                'rol' => 'admin',
                 'idusuario' => $user->idcliente // agrega esta línea
             ]);            
         } else {
