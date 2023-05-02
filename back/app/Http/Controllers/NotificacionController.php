66e0bad24a7e7b8ef0549876b66c0d1b3494cf71
IvanD-A <alarconivan73@gmail.com>
2023-05-01 23:08:15 -0400

    index notificaciones

--- back/app/Http/Controllers/NotificacionController.php
+++ back/app/Http/Controllers/NotificacionController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 
 class NotificacionController extends Controller
 {
-    public function index(Request $request, $)
+    /*public function index(Request $request)
     {
         $user = $request->user();
         $notifications = $user->notifications()->paginate(10);
@@ -24,6 +24,62 @@ class NotificacionController extends Controller
             ];
         }
         return response()->json($response);
+    }*/
+
+    public function indexSent(Request $request)
+    {
+        $rol = $request->rol;
+        $id = $request->idemisor;
+        if($rol == "admin"){
+            $administrador = Administrador::find($id);
+            $notificaciones = DB::table('notificaciones')
+                ->where('data->emisor_notif', $administrador->nombre_administrador)
+                ->paginate(10);
+        }elseif($rol == "operador"){
+            $operador = Operador::find($id);
+            $notificaciones = DB::table('notificaciones')
+                ->where('data->emisor_notif', $operador->nombre_operador)
+                ->paginate(10);
+        }else{
+            $cliente = Cliente::find($id);
+            $notificaciones = DB::table('notificaciones')
+                ->where('data->emisor_notif', $cliente->nombre_cliente)
+                ->paginate(10);
+        }
+        $response = [];
+
+        foreach ($notificaciones as $notificacion) {
+        $response[] = $notificacion->data;
+        }
+        return response()->json($response);
+    }
+
+    public function indexReceived(Request $request)
+    {
+        $rol = $request->rol;
+        $id = $request->idreceptor;
+        if($rol == "admin"){
+            $administrador = Administrador::find($id);
+            $notificaciones = DB::table('notificaciones')
+                ->where('data->receptor_notif', $administrador->nombre_administrador)
+                ->paginate(10);
+        }elseif($rol == "operador"){
+            $operador = Operador::find($id);
+            $notificaciones = DB::table('notificaciones')
+                ->where('data->receptor_notif', $operador->nombre_operador)
+                ->paginate(10);
+        }else{
+            $cliente = Cliente::find($id);
+            $notificaciones = DB::table('notificaciones')
+                ->where('data->receptor_notif', $cliente->nombre_cliente)
+                ->paginate(10);
+        }
+        $response = [];
+
+        foreach ($notificaciones as $notificacion) {
+        $response[] = $notificacion->data;
+        }
+        return response()->json($response);
     }
 
     public function store(Request $request)
@@ -31,22 +87,22 @@ class NotificacionController extends Controller
         $notificacion = new Notificacion();
         $notificacion->emisor_notif = Auth::user()->name;
         $notificacion->mensaje_notif = $request->input('mensaje_notif');
-        $notificacion->administrador_idadministrador = $request->input('administrador_idadministrador');
+        $notificacion->operador_idoperador = $request->input('operador_idoperador');
         $notificacion->cliente_idcliente = $request->input('cliente_idcliente');
         $notificacion->save();
 
         $receptor = null;
-        if ($notificacion->administrador_idadministrador) {
-            $receptor = 'administrador';
+        if ($notificacion->operador_idoperador) {
+            $receptor = 'operador';
         } elseif ($notificacion->cliente_idcliente) {
             $receptor = 'cliente';
         }
 
         if ($receptor) {
             $mensaje = [
-                'id_notificacion' => $notificacion->idnotificaciones,
-                'mensaje_notif' => $notificacion->mensaje_notif,
-                'emisor_notif' => $notificacion->emisor_notif,
+                'id_notificacion' => $notificaciones->idnotificaciones,
+                'mensaje_notif' => $notificaciones->mensaje_notif,
+                'emisor_notif' => $notificaciones->emisor_notif,
             ];
             $notificacionReceptor = new NotificacionNueva($mensaje);
             Notification::route('broadcast', $receptor)->notify($notificacionReceptor);
@@ -55,7 +111,7 @@ class NotificacionController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'Notificación creada exitosamente',
-            'data' => $notificacion
+            'data' => $notificaciones
         ]);
     }
 }
