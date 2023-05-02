a246c05bd4e880dd05d003c0a557786635577852
DanielaEstefany <estefanymunoz0@gmail.com>
2023-05-01 20:18:05 -0400

    merge

--- back/vendor/composer/installed.php
+++ back/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '4e9ef8b433883aa67203519a91531b6668daa214',
+        'reference' => 'bc63143261466446d904e07fb6e92309ff21cf7f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -406,7 +406,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '4e9ef8b433883aa67203519a91531b6668daa214',
+            'reference' => 'bc63143261466446d904e07fb6e92309ff21cf7f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -730,8 +730,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0.0 || 2.0.0 || 3.0.0',
-                1 => '1.0|2.0',
+                0 => '1.0|2.0',
+                1 => '1.0.0 || 2.0.0 || 3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
